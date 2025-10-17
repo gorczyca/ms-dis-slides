a9bdbbce4ef17e7f188cf75e1e2b104293060ac3
Piotr Gorczyca <piotr.gorczyca@tu-dresden.de>
2025-10-17 14:26:19 +0200

    Add skeleton for evaluation setup

--- slides/conclusion.py
+++ slides/conclusion.py
@@ -20,20 +20,6 @@ class Conclusion(BaseSlide):
             r'\sffamily -- say we have also variant for different semantics',
             r'\sffamily -- we also have graphical interface, interactive',
             r'\sffamily -- future work',
-            # r'\sffamily (1) initialization (the entire base)',
-            # r'\sffamily (2) update state (explain and maybe show visually culprits, defences, blocked pieces)',
-            # r'\sffamily (3) possible moves',
-            # r'\sffamily (4) new pieces from the performed moves',
-            # r'\sffamily (5) termination criteria',
-            # r'\sffamily (6) choice of next move',
-
-            # r'\sffamily -- show AF (from previous slide)',
-            # r'\sffamily -- go line by line and show what is encoded and how this corresponds to AF components',
-            # r'\sffamily -- also show the various parts (base, updateState, step)',
-            # r'\sffamily -- explain what dispute state is in AF',
-            # r'\sffamily -- lightweight, modular, declarative, extensible implementation',
-            # r'\sffamily -- advance ABA disputes',
-            # r'\sffamily -- propose our approach as general methodology for argument games',
             font_size=30, color=BLACK, buff=0.2)
 
         s.add(bullets)
